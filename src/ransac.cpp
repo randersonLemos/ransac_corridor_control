19b7f95df76481993d6e0d0389f878ed5b5cd787
Randerson Araujo de Lemos <ranarelemos@msn.com>
2016-08-24 13:23:10 -0300

    Removed unnecessary variables, changed some codes and added several comments.

--- src/ransac.cpp
+++ src/ransac.cpp
@@ -22,14 +22,13 @@
 
 void laser::laserCallback(const sensor_msgs::LaserScan& msg){
     //watchdog->IsAlive();
-    std::vector<float> x_left, x_right, y_left, y_right;
-    std::vector<float> x_l, x_r, y_l, y_r, lineR(3), lineL(3);
-    float modelL[3];
-    float modelR[3];
+    std::vector<float> x_left, x_right, y_left, y_right; // points inside window of interest
+    // holder of the coefficients of the lines of interest
+    // the coefficients are from the line equation ax + by + c = 0;
+    float modelL[3], modelR[3];
     float **dR, **dL;
+    int ret, inliersR, inliersL;
     float theta;
-    int ret, inliersR, inliersL, i;
-
     tf::TransformListener listener(ros::Duration(10));
     geometry_msgs::PointStamped laser_point;
     laser_point.header.stamp = ros::Time(0);
@@ -48,22 +47,22 @@ void laser::laserCallback(const sensor_msgs::LaserScan& msg){
             /* car angle -> trajectory angle */
             if(winAngle){
                 laser_point.point.x =  cos(winAngle)*laser_point.point.x +
-                    sin(winAngle)*laser_point.point.y;
+                                       sin(winAngle)*laser_point.point.y;
                 laser_point.point.y = -sin(winAngle)*laser_point.point.x +
-                    cos(winAngle)*laser_point.point.y;
+                                       cos(winAngle)*laser_point.point.y;
             }
 
             try{
                 geometry_msgs::PointStamped vero_point;
                 listener.waitForTransform(BASE_LINK_FRAME_ID, LASER_FRAME_ID,
                                           ros::Time(0), ros::Duration(1.0));
-                listener.transformPoint(BASE_LINK_FRAME_ID,
-                                        laser_point, vero_point);
+                listener.transformPoint(BASE_LINK_FRAME_ID, laser_point, vero_point);
 
                 if(laser_point.point.x < (winLength * dataWidth) &&
                 abs(laser_point.point.y) < (winWidth * dataWidth)){
-                    if(laser_point.point.y > 0){
+                    if(laser_point.point.y > 0){ // points of the left line
                         if(winAngle){
+                            /* trajectory angle -> car angle*/
                             x_left.push_back(cos(winAngle)*vero_point.point.x -
                                              sin(winAngle)*vero_point.point.y);
                             y_left.push_back(sin(winAngle)*vero_point.point.x +
@@ -74,8 +73,9 @@ void laser::laserCallback(const sensor_msgs::LaserScan& msg){
                             y_left.push_back(vero_point.point.y);
                         }
                     }
-                    else if(laser_point.point.y < 0){
+                    else if(laser_point.point.y < 0){ // points of the rigth line
                         if(winAngle){
+                            /* trajectory angle -> car angle*/
                             x_right.push_back(cos(winAngle)*vero_point.point.x -
                                               sin(winAngle)*vero_point.point.y);
                             y_right.push_back(sin(winAngle)*vero_point.point.x +
@@ -100,39 +100,14 @@ void laser::laserCallback(const sensor_msgs::LaserScan& msg){
             }
         }
     }
-    for(std::vector<float>::size_type j = 0; j != x_left.size(); ++j){
-        x_l.push_back(x_left[j]);
-        y_l.push_back(y_left[j]);
-    }
 
-    for(std::vector<float>::size_type j = 0; j != x_right.size(); ++j){
-        x_r.push_back(x_right[j]);
-        y_r.push_back(y_right[j]);
-    }
-
-    dR = (float **) malloc(x_right.size() * sizeof(float *));
-    if(dR == NULL){
-        perror("out of memory\n");
-        exit(0);
-    }
-
-    for(i = 0; i < x_right.size(); i++){
-        dR[i] = (float *) malloc(2 * sizeof(float));
-        if(dR[i] == NULL){
-            perror("out of memory\n");
-            exit(0);
-        }
-        dR[i][0] = x_right[i];
-        dR[i][1] = y_right[i];
-    }
-
+    /* Setting variables for the function ransac_2Dline */
     dL = (float **) malloc(x_left.size() * sizeof(float *));
     if(dR == NULL){
         perror("out of memory\n");
         exit(0);
     }
-
-    for(i = 0; i < x_left.size(); i++){
+    for(std::vector<float>::size_type i = 0; i < x_left.size(); ++i){
         dL[i] = (float *) malloc(2 * sizeof(float));
         if(dL[i] == NULL){
             perror("out of memory\n");
@@ -142,52 +117,65 @@ void laser::laserCallback(const sensor_msgs::LaserScan& msg){
         dL[i][1] = y_left[i];
     }
 
+    dR = (float **) malloc(x_right.size() * sizeof(float *));
+    if(dR == NULL){
+        perror("out of memory\n");
+        exit(0);
+    }
+    for(std::vector<float>::size_type i = 0; i < x_right.size(); ++i){
+        dR[i] = (float *) malloc(2 * sizeof(float));
+        if(dR[i] == NULL){
+            perror("out of memory\n");
+            exit(0);
+        }
+        dR[i][0] = x_right[i];
+        dR[i][1] = y_right[i];
+    }
+
     ret  = ransac_2Dline(dR, x_right.size(), (x_right.size()/2)-1, threshold, modelR, &inliersR, verbose);
     ret += ransac_2Dline(dL, x_left.size(), (x_left.size()/2)-1, threshold, modelL, &inliersL, verbose);
 
-    for(i = 0; i < x_right.size(); i++){
-        free(dR[i]);
-    }
-    free(dR);
-
-    for(i = 0; i < x_left.size(); i++){
+    for(std::vector<float>::size_type i = 0; i < x_left.size(); ++i){
         free(dL[i]);
     }
     free(dL);
 
+    for(std::vector<float>::size_type i = 0; i < x_right.size(); ++i){
+        free(dR[i]);
+    }
+    free(dR);
+
     if(ret == 0){
-        lineR[0] = modelR[0];
-        lineR[1] = modelR[1];
-        lineR[2] = modelR[2];
-
-        lineL[0] = modelL[0];
-        lineL[1] = modelL[1];
-        lineL[2] = modelL[2];
-
-        std::vector<double> biModel = bisectrixLine(lineL, lineR);
-        std::vector<float> bi2msg;
-        ransac_project::Bisectrix biMsg;
-
-        for(int j = 0; j < biModel.size(); j ++){
-              bi2msg.push_back(biModel[j]);
+        // The arguments of the function bisectrixLine are float vectors.
+        // Lets "cast" the array variable modelL/R to a float vector.
+        std::vector<float> lineR(3), lineL(3);
+        for(std::vector<float>::size_type i = 0; i!=lineL.size(); ++i){
+            lineL[i] = modelL[i];
+            lineR[i] = modelR[i];
         }
 
+        // Bisectrix coefficients
+        std::vector<double> lineBi = bisectrixLine(lineL, lineR);
+
+        // The vector ros messagers are float vector. 
+        // Lets "cats" the double vector lineBi to a float vector.
+        // The cast is necessary in order to plublish this variable.
+        std::vector<float> bi2msg(lineBi.begin(), lineBi.end());
+        ransac_project::Bisectrix biMsg;
         biMsg.bisectrix = bi2msg;
-        pub_bisec->publish(biMsg);
+        pub_bisec->publish(biMsg); //publishing the coefficients of the bisect
 
         last_winAngle = winAngle;
-        last_trajAngle = atan(-biModel[0]/biModel[1]); // inclinacao da bissetriz estimada, em radianos
-        // outFile << atan(-biModel[0]/biModel[1]) << "\n";
+        last_trajAngle = atan(-lineBi[0]/lineBi[1]); // slop of the estimated bisect
 
-        //x_l, y_l, x_r, y_r sao pontos apenas para a visualização utilizados pelo node dataplot
         ransac_project::BorderLines msg_pub;
         msg_pub.line_left = lineL;
         msg_pub.line_right = lineR;
-        msg_pub.x_left = x_l;
-        msg_pub.y_left = y_l;
-        msg_pub.x_right = x_r;
-        msg_pub.y_right = y_r;
-        pub_lines->publish(msg_pub);
+        msg_pub.x_left = x_left;
+        msg_pub.y_left = y_left;
+        msg_pub.x_right = x_right;
+        msg_pub.y_right = y_right;
+        pub_lines->publish(msg_pub); // publishing coefficients of the left and righ lines
     }
 }
 
